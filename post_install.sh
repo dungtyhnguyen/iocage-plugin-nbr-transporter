e275a58d3372722357172bb99be7579b9dc76b57
dungtyhnguyen <88713322+dungtyhnguyen@users.noreply.github.com>
2021-10-26 16:52:27 +0700

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -14,14 +14,14 @@ if [ $? -ne 0 -o ! -e $INSTALL ]; then
     exit 1
 fi
 
-CHECKSUM=`sha256 -q $INSTALL`
-if [ "$SHA256" != "$CHECKSUM" ]; then
-    echo "ERROR: Incorrect $PRODUCT installer checksum"
-    rm $INSTALL >/dev/null 2>&1
-    exit 2
-fi
+# CHECKSUM=`sha256 -q $INSTALL`
+# if [ "$SHA256" != "$CHECKSUM" ]; then
+#     echo "ERROR: Incorrect $PRODUCT installer checksum"
+#     rm $INSTALL >/dev/null 2>&1
+#     exit 2
+# fi
 
-sh ./$INSTALL -s -i "$PRODUCT_ROOT" --eula-accept 2>&1
+sh ./$INSTALL -f -y -i "$PRODUCT_ROOT" --eula-accept --extract 2>&1
 if [ $? -ne 0 ]; then
     echo "ERROR: $PRODUCT install failed"
     rm $INSTALL >/dev/null 2>&1
@@ -29,4 +29,16 @@ if [ $? -ne 0 ]; then
 fi
 rm $INSTALL >/dev/null 2>&1
 
-exit 0
+#disable default HTTP ports redirect
+SVC_PATH="$PRODUCT_ROOT/director"
+awk 'BEGIN{A=0} /port="80/{A=1} {if (A==0) print $0} />/{A=0}' $SVC_PATH/tomcat/conf/server-linux.xml >$SVC_PATH/tomcat/conf/server-linux.xml_ 2>/dev/null
+mv $SVC_PATH/tomcat/conf/server-linux.xml_ $SVC_PATH/tomcat/conf/server-linux.xml >/dev/null 2>&1
+
+#enforce EULA
+PROFILE=`ls "$SVC_PATH/userdata/"*.profile 2>/dev/null | head -1`
+if [ "x$PROFILE" != "x" ]; then
+    sed -e 's@"system.licensing.eula.must.agree": false@"system.licensing.eula.must.agree": true@' "$PROFILE" >"${PROFILE}_" 2>/dev/null
+    mv "${PROFILE}_" "$PROFILE" >/dev/null 2>&1
+fi
+
+service nkv_dirsvc start >/dev/null 2>&1
